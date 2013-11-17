f2537383b176cea2237b784ce1b1826f038cd9a7
Joel Burget <joel@khanacademy.org>
2013-11-16 22:34:48 -0800

    Update configure.ac.
    The other changes are a result of running autoconf and configure.

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
-SDL_CFLAGS=-I/usr/local/include/SDL -D_GNU_SOURCE=1 -D_THREAD_SAFE
+SDL_CFLAGS=-I/usr/local/include/SDL2 -D_THREAD_SAFE
 SDL_LIBS=-lSDL_gfx
-PACKAGE=SDL-gfx
+PACKAGE=sdl2-gfx
 VERSION=0.1.0
 MAINTAINER=joelburget@gmail.com
